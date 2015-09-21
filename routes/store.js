8ace2547f8ebe7a532f84dba79b41ff6c64db019
unknown <hmoy@LT-738524.statoil.net>
2015-09-21 14:14:10 +0200

    ny

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('store');
 });
+
+router.route('/message')
+     .post(function(req, res, next){
+       console.log('Receiving message '+ req.body.message);
+       res.send('Message was ' + req.body.message);
+     } );
 module.exports = router;
