7ae682b52e0f7f554eca871ac657ce0338c2b3d8
unknown <hmoy@LT-738524.statoil.net>
2015-09-21 16:18:45 +0200

    m db

--- routes/store.js
+++ routes/store.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
+var mongoURL = 'mongodb://messagewriter:' + (process.env.DBPASSWORD || ' empty') + '@ds051913.mongolab.com:51913/hege';
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -8,9 +10,47 @@ router.get('/', function(req, res, next) {
         messageURL: process.env.MESSAGE_URL});
 });
 
+
 router.route('/message')
-     .post(function(req, res, next){
-       console.log('Receiving message '+ req.body.message);
-       res.send('Message was ' + req.body.message);
-     } );
+  .post(function (req, res, next) {
+    var txtMessage = (req.body.message || 'empty message');
+ // Storing message in database
+ 
+   MongoClient.connect(mongoURL, function(err, db) {
+   console.log("Connected to database");
+
+   db.collection('messages').insert({'message': txtMessage}, {w: 1 }, function (err, item) {
+   if (err) {
+     console.log('Error storing message in database: ' + err);
+      db.close();
+     res.status(400).send('Error, unable to store message: ' + txtMessage);
+   } else {
+     db.close();
+     console.log('Message stored ok in database: ' + txtMessage)
+     res.status(200).send('Message stored: "' + txtMessage + '"');
+   }
+   });
+ });
+ // End storing message in database
+ });
+// router.route('/message')
+//      .post(function(req, res, next){
+//        var txtMessage = (req.body.message || 'empty message' );
+//        MongoClient.connect(mongoURL, function(err, db){
+//          console.log("Connected to the db");
+//          db.collection('messages').insert({'message': txtMessage}, {w: 1}, function(err, item)){
+//            if (err){
+//              console.log('Error storing message i database: ' + err);
+//              db.close();
+//              res.status(400).send('Error, unable to store message: ' + txtMessage);
+//            } else{
+//              db.close();
+//              console.log('Message stored ok in database: '+ txtMessage + "");
+//            }
+//          });
+//        });
+       // console.log('Receiving message '+ req.body.message);
+       // res.send('Message was ' + req.body.message);
+     // } );
 module.exports = router;
+
